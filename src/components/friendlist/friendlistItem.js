25d988dde59c184d274d234081351996f40ba64d
Roman-Solovei <romario2409@gmail.com>
2022-02-06 21:15:34 +0200

    hw 01 06

--- src/components/friendlist/friendlistItem.js
+++ src/components/friendlist/friendlistItem.js
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types';
 import s from './friendlist.module.css';
 
 
-export default function FriendListItem({ avatar, name, isOnline, id }) {
-    // console.log({id})
-    return <li key={id} className={s.item}>
+export default function FriendListItem({ avatar, name, isOnline }) {
+    
+    return <li className={s.item}>
             <span className = {s[isOnline]}>{isOnline}</span>            
             <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
             <p className={s.name}>{name}</p>
@@ -14,6 +14,5 @@ export default function FriendListItem({ avatar, name, isOnline, id }) {
 FriendListItem.propTypes = {
   avatar: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  isOnline: PropTypes.bool.isRequired,
-  id: PropTypes.number.isRequired
+  isOnline: PropTypes.bool.isRequired, 
 }
