753c7f174eb8a6520450de2669042c81023e394b
Roman-Solovei <romario2409@gmail.com>
2022-02-07 21:16:13 +0200

    hw 01 06

--- src/components/friendlist/friendlistItem.js
+++ src/components/friendlist/friendlistItem.js
@@ -5,7 +5,9 @@ import s from './friendlist.module.css';
 export default function FriendListItem({ avatar, name, isOnline }) {
     
     return <li className={s.item}>
-            <span className = {s[isOnline]}>{isOnline}</span>            
+            <span className={s[isOnline]}></span>  
+      {/* выриант отображения статуса с тернарником */}
+      {/* <span className={isOnline ? s.true : s.false}></span>  */}
             <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
             <p className={s.name}>{name}</p>
             </li>
